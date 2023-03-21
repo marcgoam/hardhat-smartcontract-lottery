18b3aeb26b054c5f730af705bdbd40e7ecd3351e
Marc González <marcgoam@DESKTOP-2NSFJTV>
2023-03-21 12:31:04 +0100

    First Commit

---
+++ contracts/Raffle.sol
@@ -0,0 +1,6 @@
+// Raffle
+
+// Enter the lottery (paying some amount)
+// Pick a random winner (ChainLink randomness)
+// Winner to be selected every X minutes -> completly automated
+// ChainLink oracle -> Randomness, Automated execution (Chainlink Keepers)
